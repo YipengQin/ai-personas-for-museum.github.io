51317ba657681fc5b4df2e8b695fd3c9c7511dd2
charlieJ107 <charlie_j107@outlook.com>
2024-02-20 23:54:42 +0000

    Refactor with custom template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import tailwind from "@astrojs/tailwind";
 
-import sitemap from '@astrojs/sitemap';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  integrations: [tailwind(), mdx()]
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#000044',
+                secondary: '#021d60',
+                white: '#ffffff',
+                black: '#000000',
+                gray: {
+                    100: '#f7f7f7',
+                    200: '#f0f0f0',
+                    300: '#e3e3e3',
+                    400: '#c1c1c1',
+                    500: '#a1a1a1',
+                    600: '#6e6e6e',
+                    700: '#4a4a4a',
+                    800: '#2d2d2d',
+                    900: '#1a1a1a',
+                },
+            },
+            backgroundColor: '#021d60',
+            textColor: "#ffffff"
+        },
+    },
+    plugins: [
+        require('@tailwindcss/typography')
+    ],
+}
